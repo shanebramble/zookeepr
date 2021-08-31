748dc1ae653b43cfa4da7f95cf9b1e1be2c157dd
Shane Bramble-Wade <sbramwade@msn.com>
2021-08-31 15:58:29 -0400

    organized and modularized each functon and routes

--- server.js
+++ server.js
@@ -1,157 +1,20 @@
-const {
-    query
-} = require('express');
 const express = require('express');
-const {
-    animals
-} = require('./data/animals.json');
-const fs = require('fs');
-const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-// We need to instantiate the server, then tell it to listen for requests.
 const app = express();
-// parse incoming string or array data
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({
     extended: true
 }));
-// parse incoming JSON data
 app.use(express.json());
-// we can set up some more Express.js middleware that instructs the server to make 
-// certain files readily available and to not gate it behind a server endpoint.
-
-// We added some more middleware to our server and used the express.static() method.
-// The way it works is that we provide a file path to a location in our application( in this
-// case, the public folder) and instruct the server to make these files static 
-// resources.This means that all of our front - end code can now be accessed 
-// without having a specific server endpoint created for it!
-
 app.use(express.static('public'));
 
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        // Save personalityTraits as a dedicated array.
-        // If personalityTraits is a string, place it into a new array and save.
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        // Loop through each trait in the personalityTraits array:
-        personalityTraitsArray.forEach(trait => {
-            // Check the trait against each animal in the filteredResults array.
-            // Remember, it is initially a copy of the animalsArray,
-            // but here we're updating it for each trait in the .forEach() loop.
-            // For each trait being targeted by the filter, the filteredResults
-            // array will then contain only the entries that contain the trait,
-            // so at the end we'll have an array of animals that have every one 
-            // of the traits when the .forEach() loop is finished.
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-}
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-
-    fs.writeFileSync(path.join(__dirname, './data/animals.json'),
-        JSON.stringify({
-            animals: animalsArray
-        }, null, 2)
-    );
-
-    return animal;
-}
-// Validation of the input fields
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-}
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-
-    if (req.query) {
-        results = filterByQuery(req.query, results)
-    }
-    res.json(results);
-});
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        // add animal to json file and animals array in this function
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
-
-});
-// Unlike most GET and POST routes that deal with creating or return JSON data, this
-// GET route has just one job to do, and that is to respond with an HTML page to 
-// display in the browser. So instead of using res.json(), we're using res.sendFile(),
-// and all we have to do is tell them where to find the file we want our server to 
-// read and send back to the client.
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-// A wildcard route used for when a user types an incorrect route.
-// The * will act as a wildcard, meaning any route that wasn 't previously defined 
-// will fall under this request and will receive the homepage as the response. 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
